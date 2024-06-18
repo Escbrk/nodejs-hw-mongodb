73987022819685bfae6cb3b8f15b245c3a03b2de
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-06-17 18:21:07 -0600

    fixed

--- src/validation/createContactsSchema.js
+++ src/validation/createContactsSchema.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 export const createContactsSchema = Joi.object({
   name: Joi.string().required().min(3).max(20),
-  phoneNumber: Joi.number().required().integer(),
+  phoneNumber: Joi.string().required().min(3).max(20),
   email: Joi.string().min(3).max(20),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal'),
--- src/validation/updateContactsSchema.js
+++ src/validation/updateContactsSchema.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 export const updateContactsSchema = Joi.object({
   name: Joi.string().min(3).max(20),
-  phoneNumber: Joi.number().integer(),
+  phoneNumber: Joi.string().min(3).max(20),
   email: Joi.string().min(3).max(20),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal'),
